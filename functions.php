591234bd2e21dd06814b87b0c66ebd9796affed3
ciokka <cristiano@moesli.it>
2025-03-13 09:30:02 +0100

    attivo modalità manutenzione

--- functions.php
+++ functions.php
@@ -217,7 +217,7 @@ add_action('admin_bar_menu', 'aggiungi_voce_wpadminbar', 999);
 
 function wp_maintenance_mode()
 {
-	if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+	if (!current_user_can('administrator')) {
 		wp_die('
         <h1>Stiamo aggiornando il nostro sito.</h1>
         <p>Sul sito sono in corso degli aggiornamenti. Si prega di ritornare più tardi.</p> ', 'Sito in modalità manutenzione');
